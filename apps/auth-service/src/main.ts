b99816b9cb65caae2b3b472c5336d79787784aa4
Komla Adzam <adzamkomla.dev@gmail.com>
2022-07-24 19:00:37 +0000

    Add grpc support

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -5,11 +5,22 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import { join } from 'path';
 
 import { AppModule } from './app/app.module';
 import {environment} from "./environments/environment";
 
 async function bootstrap() {
+  const microservice = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'auth',
+      protoPath: join(__dirname, 'auth/auth.proto'),
+    },
+  });
+  await microservice.listen();
+
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
