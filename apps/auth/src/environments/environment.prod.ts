fcbdfed9bf2ba9689ad44d1679a9863b2e3fd6c7
Komla Adzam <adzamkomla.dev@gmail.com>
2022-12-04 16:36:35 +0000

    update base URL

--- apps/auth/src/environments/environment.prod.ts
+++ apps/auth/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   gateways: {
     users: {
-      baseUrl: process.env['NX_GATEWAYS_USERS_URL'],
+      baseUrl: process.env['NX_GATEWAYS_USERS_BASE_URL'],
       gqlUrl: `${process.env['NX_GATEWAYS_USERS_URL']}`,
     },
   },
--- apps/auth/src/environments/environment.ts
+++ apps/auth/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   gateways: {
     users: {
-      baseUrl: process.env['NX_GATEWAYS_USERS_URL'],
+      baseUrl: process.env['NX_GATEWAYS_USERS_BASE_URL'],
       gqlUrl: `${process.env['NX_GATEWAYS_USERS_URL']}`,
     },
   },
