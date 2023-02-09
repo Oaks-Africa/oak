eeb36937ea17c5625750c6d88d56c8c767fd3039
Datsomor Gerhardt <datsomorg@gmail.com>
2023-02-09 09:41:07 +0100

    add sign in forms

--- apps/auth/src/app/sign-in/sign-in.component.ts
+++ apps/auth/src/app/sign-in/sign-in.component.ts
@@ -5,11 +5,18 @@ import { Observable } from 'rxjs';
 
 import { AuthState, AuthQuery, AuthService } from '@oak/auth/data-access';
 import { RouterLink } from "@angular/router";
+import { SignInFormComponent } from './sign-in-form/sign-in-form.component';
+import { TuiIslandModule } from '@taiga-ui/kit';
 
 @Component({
   selector: 'oak-sign-in',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule, 
+    RouterLink,
+    TuiIslandModule,
+    SignInFormComponent,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,7 +35,7 @@ export class SignInComponent {
     this.allState$ = this.authQuery.allState$;
   }
 
-  onSignInViaEmail() {
+  onSignInViaEmail(data: any) {
     this.authService.signInViaEmail('pand@gmail.com', 'aBcdef12#$#$');
   }
 }
