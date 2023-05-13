d66fd3851f3f6b36e4359770bdd17fccaa2e2927
Komla Adzam <adzamkomla.dev@gmail.com>
2023-05-13 08:52:56 +0000

    save progress

--- apps/services.notifications/src/app/app.module.ts
+++ apps/services.notifications/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GlobalModule } from './@common/modules/global.module';
 import { UsersModule } from './users/users.module';
+import { OtpModule } from './otp/otp.module';
 
 @Module({
-  imports: [GlobalModule, UsersModule],
+  imports: [GlobalModule, UsersModule, OtpModule],
   controllers: [AppController],
   providers: [AppService],
 })
